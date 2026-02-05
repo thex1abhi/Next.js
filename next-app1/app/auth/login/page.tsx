356ebb11bbc3b37771d040b30f1e3f98473f41fc
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-05 23:35:59 +0530

    Login page done

--- next-app1/app/auth/login/page.tsx
+++ next-app1/app/auth/login/page.tsx
@@ -1,9 +1,71 @@
+"use client";
 
-export default function Login(){
+import { loginSchema } from "@/app/Schemas/auth"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
+
+export default function Login() {
+    const form = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: ""
+        }
+    })
+
+
+    async function onSubmit() {
+        console.log("done");
+
+    }
     return (
         <>
-        <div> 
-            Login  page
-            </div> </>
+            <div>
+                <Card>
+                    <CardHeader  >
+                        <CardTitle> Login </CardTitle>
+                        <CardDescription>Please login to Continue</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form onSubmit={form.handleSubmit(onSubmit)}  >
+                            <FieldGroup className="gap-y-4" >
+
+
+                                <Controller name="email" control={form.control} render={({ field, fieldState }) => (
+                                    <Field>
+                                        <FieldLabel>  Email</FieldLabel>
+                                        <Input aria-invalid={fieldState.invalid}   {...field} placeholder="example@mail.com" />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} ></FieldError>
+                                        )}
+                                    </Field>
+                                )} />
+
+                                <Controller name="password" control={form.control} render={({ field, fieldState }) => (
+                                    <Field>  <FieldLabel>  Password</FieldLabel>
+                                        <Input aria-invalid={fieldState.invalid} placeholder="********" {...field} />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} ></FieldError>
+                                        )}
+                                    </Field>
+                                )} />
+                                <Button>Login </Button>
+
+                            </FieldGroup>
+
+
+
+
+
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
     )
 }
--- next-app1/components/Web/ConvexClientProvider.tsx
+++ next-app1/components/Web/ConvexClientProvider.tsx
@@ -5,7 +5,9 @@ import { ConvexReactClient } from "convex/react";
 import { authClient } from "@/lib/auth-client";
 import { ConvexBetterAuthProvider } from "@convex-dev/better-auth/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!,{
+  expectAuth:true,
+});
 
 export function ConvexClientProvider({
   children,
