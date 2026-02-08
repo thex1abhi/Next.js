1d4ff8c568f8dbdb45382ccae70af953e92a2206
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-08 19:11:01 +0530

    Learned Skeleton/streaming

--- next-app1/app/(shared-layout)/blog/loading.tsx
+++
@@ -1,5 +0,0 @@
-export default function loadingBlog() {
-    return (
-    <h1  className="text-5xl font-bold text-red-500 p-10 " > Loading...</h1>
-    )
-}
--- next-app1/app/(shared-layout)/blog/page.tsx
+++ next-app1/app/(shared-layout)/blog/page.tsx
@@ -2,47 +2,79 @@
 
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { useQuery } from "convex/react";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
 
-export default async  function BlogPage() { 
-    await new Promise ((resolve)=>setTimeout(resolve,5000));
+export default function BlogPage() {
+
+
+    return (
+        <>
+            <div className="flex justify-center text-2xl  " >
+                <div  >   Our blog  </div>
+                <div  >  Insights,thoughts and trends from our team </div>
+            </div>
+            <Suspense fallback={
+                <SkeletonLoadingUI />
+
+            } >
+                < LoadBlogList />
+            </Suspense>
+        </>
+    )
+}
+
+async function LoadBlogList() {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
     const data = await fetchQuery(api.posts.getPosts);
 
+    return (
+        <div className="grid mt-5 gap-6 md:grid-cols-2 lg:grid-cols-3" >
 
-
-    return (  
-        <>
-         <div  className="flex justify-center text-2xl  " >
-          <div  >   Our blog  </div> 
-          <div  >  Insights,thoughts and trends from our team </div> 
-        </div> 
-        <div className="grid mt-5 gap-6 md:grid-cols-2 lg:grid-cols-3" > 
-       
             {data?.map((post) => (
                 <Card key={post._id} className="pt-0" >
                     <div className="relative h-48 w-full overflow-hidden " >
                         <Image src="https://images.unsplash.com/photo-1770287691979-6b8ec41f5331?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt="error" fill className="rounded-t-lg" ></Image>
-                    </div> 
+                    </div>
                     <CardContent>
                         <Link href={`/blog/${post._id}`}>  <h1 className="" >{post.title} </h1>
-                        </Link> 
+                        </Link>
                         <h3> {post.body} </h3>
-                    </CardContent> 
+                    </CardContent>
 
                     <CardFooter >
-                    <Link className={buttonVariants({
-                        className:"w-full"
-                    })}  href={`/blog/${post._id}`}>  Read More 
-                        </Link> 
+                        <Link className={buttonVariants({
+                            className: "w-full"
+                        })} href={`/blog/${post._id}`}>  Read More
+                        </Link>
                     </CardFooter>
                 </Card>
             ))}
-        </div> 
-        </>
+        </div>
     )
+
 }
+
+function SkeletonLoadingUI() {
+
+    return (
+        <div className="grid mt-5 gap-6 md:grid-cols-2 lg:grid-cols-3" >
+            {[
+                ...Array(3)].map((_, i) => (<div className="flex flex-col space-y-3 " key={i} >
+                    <Skeleton className="h-48 w-full rounded-xl" />
+                    <div className="space-y-2 flex flex-col " >
+                        <Skeleton className="h-6 w-3/4" />
+                        <Skeleton className="h-4 w-full" />
+                        
+                    </div>
+                </div>))
+            } 
+        </div>
+    )
+} 
