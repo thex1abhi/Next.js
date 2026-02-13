e3fe5651358c9fa2264dde69a5c5b0e668c6cec2
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-13 23:54:45 +0530

    Project completed

--- next-app1/components/Web/navbar.tsx
+++ next-app1/components/Web/navbar.tsx
@@ -7,54 +7,58 @@ import { useConvexAuth } from "convex/react";
 import { authClient } from "@/lib/auth-client";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { SearchInput } from "./SearchInput";
 
-export function Navbar() { 
-   const {isAuthenticated,isLoading}=useConvexAuth() 
-   const router=useRouter();
+export function Navbar() {
+   const { isAuthenticated, isLoading } = useConvexAuth()
+   const router = useRouter();
    return (
       <>
          <nav className="w-full py-5 text-xl flex items-center  justify-between" >
 
             <div className="flex items-center gap-8  " >
-               <Link   href='/' >
+               <Link href='/' >
                   <h1 className="text-3xl font-bold " >
                      Next<span className="text-blue-500"  >Pro</span>
                   </h1> </Link>
                <div className="flex items-center gap-2 " >
-                  <Link className={buttonVariants({ variant:"ghost"})} href="/" > Home  </Link>
-                  <Link className={buttonVariants({ variant:"ghost"})} href="/blog" >Blog  </Link>
-                  <Link className={buttonVariants({ variant:"ghost"})} href="/create" >Create  </Link>
+                  <Link className={buttonVariants({ variant: "ghost" })} href="/" > Home  </Link>
+                  <Link className={buttonVariants({ variant: "ghost" })} href="/blog" >Blog  </Link>
+                  <Link className={buttonVariants({ variant: "ghost" })} href="/create" >Create  </Link>
                </div>
-            </div> 
+            </div>
 
-         <div className="flex items-center gap-2 " >  
- 
+            <div className="flex items-center gap-2 " >
+               <div className="hidden md:block  mr-2" >
 
- {isLoading ? null:isAuthenticated ? (
-   <Button   className="cursor-pointer scale-y-95"
-    onClick={()=>authClient.signOut({
-      fetchOptions:{
-         onSuccess:()=>{
-            toast.success("Logout Successfully") 
-            router.push("/")
-         },
-         onError:(error)=>{
-            toast.error(error.error.message)
-         }
-      }
-   })} >Logout </Button>
- ) : (  
-   <>
-  <Link className={buttonVariants()} href="/auth/sign-up"   > SignUp  </Link>
-            <Link className={buttonVariants({ variant:"secondary"})} href="/auth/login" > Login </Link> 
-              </>
- ) }
+                  <SearchInput />
+               </div>
 
-          
-<ThemeToggle/>
+               {isLoading ? null : isAuthenticated ? (
+                  <Button className="cursor-pointer scale-y-95"
+                     onClick={() => authClient.signOut({
+                        fetchOptions: {
+                           onSuccess: () => {
+                              toast.success("Logout Successfully")
+                              router.push("/")
+                           },
+                           onError: (error) => {
+                              toast.error(error.error.message)
+                           }
+                        }
+                     })} >Logout </Button>
+               ) : (
+                  <>
+                     <Link className={buttonVariants()} href="/auth/sign-up"   > SignUp  </Link>
+                     <Link className={buttonVariants({ variant: "secondary" })} href="/auth/login" > Login </Link>
+                  </>
+               )}
 
-              </div>
-  
+
+               <ThemeToggle />
+
+            </div>
+
          </nav>
       </>
    )
